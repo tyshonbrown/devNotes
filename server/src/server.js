f5f35dca9e036369697477210d3c33f66317ab5d
tyshonb <tbrown23@terpmail.umd.edu>
2026-06-14 11:22:47 -0400

    server fix

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://dev-notes-chi-nine.vercel.app/",
+      "https://dev-notes-chi-nine.vercel.app",
     ],
     credentials: true,
   })
